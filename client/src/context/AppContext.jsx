b3b6cf06d363d45ec194fd55623fae2d9806e60b
Abhishek-raj093 <ss0986825@gmail.com>
2025-01-26 10:37:30 +0530

    Third Commit

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { createContext } from "react";
+import { dummyCourses } from '../assets/assets';
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
 
-    const value = {
+    const currency = import.meta.env.VITE_CURRENCY
 
+    const [allCourses, setAllCourses] = useState([])
+
+    // Fetch all courses
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
+    useEffect(()=>{
+        fetchAllCourses()
+    },[])
+
+    const value = {
+        currency, allCourses
     }
 
     return (
